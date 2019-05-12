d0df0355173b182c57ce1e5af9bf93027f9af349
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-05-12 19:52:49 +0900

    (WIP) channel member action implementing. using wscat for test occurs strange results...

--- tools/shareable_ws_signaling_serv.py
+++ tools/shareable_ws_signaling_serv.py
@@ -1,87 +1,105 @@
+
 import os
 from geventwebsocket.handler import WebSocketHandler
 from gevent import pywsgi, sleep
 import traceback
 
-# from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
-# from collections import OrderedDict
+ws_list = []
+channel_dict = {}
 
-ws_set = set()
+class Channel(object):
 
-# user_hash = {} # ws => name
-# user_list = [] # contains names
+    def __init__(self, channel_sig):
+        self.users = []
+        self.channel_sig = channel_sig
 
-def accept_and_later_msg_handle(environ, start_response):
-    # global user_list
-    # global user_hash
+    def join(self, user_ws):
+        if user_ws not in self.users:
+            if len(self.users) < 2:
+                print("joined to " + self.channel_sig)
+                self.users.append(user_ws)
+            else:
+                print("chanel member capacity is already full")
+                raise Exception("chanel member capacity is already full")
 
-    #TODO: need code checking whether each connection is alive by heartbeat msg send on signaling_app func
-    global ws_set
+    def remove(self, user_ws):
+        if user_ws in self.users:
+            self.users.remove(user_ws)
 
-    ws = environ['wsgi.websocket']
-    ws_set.add(ws)
-    print('new client connected! member-num =>' + str(len(ws_set)))
-    remove_list = []
-    while True:
-        if len(remove_list) > 0:
-            for s in remove_list:
-                ws_set.remove(s)
-            remove_list = []
-
-        msg = ws.receive()
-        if msg is None:
-            break
-        for s in ws_set:
+    def delegate_msg(self, user_ws, msg):
+        print("msg delegated: " + msg)
+        remove_list = []
+        for s in self.users:
             try:
-                s.send(msg)
+                if s != user_ws:
+                    s.send(msg)
+                else:
+                    s.send("ok")
             except:
                 # Possibility to execute code when connection is closed
                 print("a client disconnected.")
                 #traceback.print_exc()
                 remove_list.append(s)
                 next
+        for s in remove_list:
+            self.users.remove(s)
+        self.dispose_if_empty()
 
-    #     user_name = msg.split(":")[0]
-    #     if (not (ws in user_hash)) or (ws in user_hash and user_hash[ws] == "init"):
-    #         user_hash[ws] = user_name
-    #         if user_name != "init":
-    #             user_list.append(user_name)
-    #     if ws in user_hash and user_name not in user_list:
-    #         if user_name != "init":
-    #             user_list.append(user_name)
-    #     remove = set()
-    #     name = msg.split(":")[0]
-    #     pure_text = msg.split(":")[1]
-    #     try:
-    #         handle_commands(name, pure_text)
-    #     except Exception:
-    #         import traceback
-    #         traceback.print_exc()
-    #         return
-    #     for s in ws_set:
-    #         try:
-    #             if s in user_hash:
-    #                 user_name = user_hash[s]
-    #             else:
-    #                 user_name = "init"
-    #
-    #             if user_name != "init":
-    #                 make_enable_open(user_list.index(user_name))
-    #             s.send(msg + "," + gen_table(user_name, msg))
-    #             make_all_close()
-    #         except Exception:
-    #             import traceback
-    #             traceback.print_exc()
-    #             remove.add(s)
-    #     for s in remove:
-    #         ws_set.remove(s)
-    # print 'exit!', len(ws_set)
+    def dispose_if_empty(self):
+        global channel_dict
+        if len(self.users) == 0:
+            del channel_dict[self.channel_sig]
 
-def clean_disconnected_client_ws_objs():
-    global ws_set
+def accept_and_later_msg_handle(environ, start_response):
+    #TODO: need code checking whether each connection is alive by heartbeat msg send on signaling_app func
+    global ws_list
+    global channel_dict
+
+    ws = environ['wsgi.websocket']
+    #print('new client connected! member-num =>' + str(len(ws_set)))
+    remove_list = []
+    while True:
+        if len(remove_list) > 0:
+            for s in remove_list:
+                ws_list.remove(s)
+            remove_list = []
+
+        msg = ws.receive()
+        if msg is None:
+            break
+
+        splited_msg = msg.split(":")
+        channel_signiture = splited_msg[0]
+        if channel_signiture.endswith("_chsig") == False:
+            print("invalid message format. no channel signiture specified.")
+            ws.send("invalid message format. no channel signiture specified.")
+
+        signaling_msg = ''.join(splited_msg[1:])
+
+        # new connection
+        if ws not in ws_list:
+            # join already existed Channel or create new Channel
+            try:
+                if channel_signiture in channel_dict:
+                    channel_dict[channel_signiture].join(ws)
+                else:
+                    new_channel = Channel(channel_signiture)
+                    new_channel.join(ws)
+                    channel_dict[channel_signiture] = new_channel
+                ws_list.append(ws)
+                channel_dict[channel_signiture].delegate_msg(ws, signaling_msg)
+            except:
+                print("join to " + channel_signiture + " is denided.")
+                ws.send("NG")
+                #ws.send("join to " + channel_signiture + " is denided.")
+                break
+
+def clean_disconnected_client_ws_objs_and_channels():
+    global ws_list
+    global channel_dict
 
     remove_list = []
-    for s in ws_set:
+    for s in ws_list:
         try:
             s.send("check_alive_msg")
         except:
@@ -91,10 +109,14 @@ def clean_disconnected_client_ws_objs():
             remove_list.append(s)
             next
     for s in remove_list:
-        ws_set.remove(s)
+        ws_list.remove(s)
+
+    dict_keys = channel_dict.keys()
+    for ch_key in dict_keys:
+        channel_dict[ch_key].dispose_if_empty()
 
 def signaling_app(environ, start_response):
-    clean_disconnected_client_ws_objs()
+    clean_disconnected_client_ws_objs_and_channels()
     path = environ["PATH_INFO"]
     if path == "/":
         return accept_and_later_msg_handle(environ, start_response)
