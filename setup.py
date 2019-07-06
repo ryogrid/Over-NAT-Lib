bdb36819a215f1a756ca27925839621b769223a3
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-07-06 11:59:10 +0900

    add configuration files for pip packaging

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os.path
+
+import setuptools
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(root_dir, 'README.md')
+with open(readme_file, encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    'aiortc-dc >= 0.5.5',
+    'gevent-websocket >= 0.10.1',
+    'websockets >= 7.0',
+    'websocket-client-py3 >= 0.15.0'
+]
+
+setuptools.setup(
+    name='onatlib',
+    version='0.0.1',
+    description='A tool offers P2P communication to transfer files and connect pipes between PCs in different NATs',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/ryogrid/Over-NAT-Lib',
+    author='Ryo Kanbayashi',
+    author_email='ryo.contact@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=['onatlib','tools'],
+    setup_requires=[],
+    install_requires=install_requires,
+)
--- tools/pync.py
+++ tools/pync.py
@@ -36,7 +36,7 @@ def distributer_loop():
         print('Waiting for connections...', file=sys.stderr)
         clientsock, client_address = server.accept() #接続されればデータを格納
 
-        with open(args.file, "rb") as f:
+        with open(args.filename, "rb") as f:
             while True:
                 data = f.read(1024)
                 #print('Received -> %s' % (rcvmsg))
@@ -113,7 +113,10 @@ def receiver_loop():
         print('Exception! Exiting.', file=sys.stderr)
         client.close()
 
-if __name__ == '__main__':
+args = None
+
+def main():
+    global args
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                 description='BHP Net Tool',
                 epilog='''\
@@ -124,23 +127,26 @@ Examples:
     python pync.py -d -p 10100 -f bar.mp4
     ''')
 
-parser.add_argument('-c', '--client', help='run as client (sender)', action='store_true')
-parser.add_argument('-s', '--server', help='listen on [host]:[port] for incoming connections', action='store_true')
-parser.add_argument('-r', '--receiver', help='run as client (receiver)', action='store_true')
-parser.add_argument('-d', '--distributer', help='listen on [host]:[port] for incoming connections and send specified file contents to client', action='store_true')
-parser.add_argument('-f', '--filename', default="", help='when send file, specify filename which is used on remote machine (contents uses data from stdin)')
-parser.add_argument('-t', '--target', default="127.0.0.1")
-parser.add_argument('-p', '--port', default=None, type=int, required=True)
-args = parser.parse_args()
+    parser.add_argument('-c', '--client', help='run as client (sender)', action='store_true')
+    parser.add_argument('-s', '--server', help='listen on [host]:[port] for incoming connections', action='store_true')
+    parser.add_argument('-r', '--receiver', help='run as client (receiver)', action='store_true')
+    parser.add_argument('-d', '--distributer', help='listen on [host]:[port] for incoming connections and send specified file contents to client', action='store_true')
+    parser.add_argument('-f', '--filename', default="", help='when send file, specify filename which is used on remote machine (contents uses data from stdin)')
+    parser.add_argument('-t', '--target', default="127.0.0.1")
+    parser.add_argument('-p', '--port', default=None, type=int, required=True)
+    args = parser.parse_args()
 
-if args.client and args.port != 0:
-    client_loop()
-elif args.receiver and args.target and args.port:
-    receiver_loop()
-elif args.server and args.port:
-    server_loop()
-elif args.distributer and args.port and args.file:
-    distributer_loop()
-else:
-    parser.print_help()
-    sys.exit(1)
+    if args.client and args.port != 0:
+        client_loop()
+    elif args.receiver and args.target and args.port:
+        receiver_loop()
+    elif args.server and args.port:
+        server_loop()
+    elif args.distributer and args.port and args.filename:
+        distributer_loop()
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
