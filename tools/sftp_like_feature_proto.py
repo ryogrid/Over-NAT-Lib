7ba9f4ea78acadc2dc62090a7eea1ea21b25205e
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-09-06 17:07:44 +0900

    added multiprocess echo server and client program.

--- tools/sftp_like_feature_proto.py
+++ tools/sftp_like_feature_proto.py
@@ -2,10 +2,13 @@
 
 from fs.osfs import OSFS
 
-home_fs = OSFS("~/")
-#print(home_fs.listdir("/"))
+#home_fs = OSFS("~/")
+home_fs = OSFS("/")
+#home_fs = OSFS("C:/")
+
+print(home_fs.listdir("../"))
 #print(home_fs.tree(max_levels = 0, dirs_first = True))
-dir_obj_infos = home_fs.scandir("/")
+dir_obj_infos = home_fs.scandir("../")
 for info in dir_obj_infos:
     if info.is_dir:
         print("[dir]  {}".format(info.name))
