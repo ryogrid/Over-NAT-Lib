e4f4f467c25cc411b2b5153cd6d238960bc814d9
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-09-15 17:06:00 +0900

    sftp like prtotype: ls and pwd of home directory worked. but ls command ouput differ between typed times. survey is needed.

--- scratch/sftp_like_feature_proto.py
+++ scratch/sftp_like_feature_proto.py
@@ -4,19 +4,30 @@ import traceback
 import sys
 import socket
 import time
+import os
 from multiprocessing import Process
+from fs.osfs import OSFS
+
+last_send_command = ""
 
 def client_handle_msg(msg):
-    print(msg.decode())
+    if last_send_command == "pwd":
+        print(msg.decode())
+    elif last_send_command == "ls":
+        print(msg.decode())
 
 def client_run():
+    global last_send_command
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         client.connect(("127.0.0.1", 11000))
         while True:
             try:
-                inputed_line = input("Please type characters and press Retuen key: ")
+                inputed_line = input("Please type command and press Retuen key: ")
+                last_send_command = inputed_line
+
                 client.sendall(inputed_line.encode())
                 recvmsg = client.recv(1024)
                 client_handle_msg(recvmsg)
@@ -28,14 +39,65 @@ def client_run():
     except:
         traceback.print_exc()
 
+receiver_fs = None
+
+def setup_pyfilesystem_object():
+    global receiver_fs
+
+    #receiver_fs = OSFS(os.getcwd())
+    receiver_fs = OSFS("~/")
+
+def handle_pwd(cmd_str):
+    url = receiver_fs.geturl("./")
+    url = url.replace("file://", "")
+    url = url.replace("\\.\\", "")
+    return url.encode()
+
+def convert_to_appropriate_size_sting(size):
+    GB = float(1024 * 1024 * 1024)
+    MB = float(1024 * 1024)
+    KB = float(1024)
+
+    formated_size = None
+    if size > GB:
+        formated_size = "{:03.4f} GB".format(size / GB)
+    elif size > MB:
+        formated_size = "{:03.4f} MB".format(size / MB)
+    elif size > KB:
+        formated_size = "{:03.4f} MB".format(size / KB)
+    else:
+        formated_size = "{:7d}  B".format(size)
+
+    return "{:>11}".format(formated_size)
+
+def handle_ls(cmd_str):
+    ret_str_file = ""
+    ret_str_dir = ""
+    dir_obj_infos = receiver_fs.scandir("./", namespaces = ["basic", "details"])
+    for info in dir_obj_infos:
+        if info.is_dir:
+            ret_str_dir += "           {0:<10}\n".format(info.name)
+        else:
+            ret_str_file += "{0} {1:<10} \n".format(convert_to_appropriate_size_sting(info.size), info.name)
+
+    return (ret_str_file + ret_str_dir).encode()
+
+# return binary data
 def server_handle_msg(msg):
-    return msg
+    global receiver_fs
+
+    cmd = msg.decode()
+    if cmd.startswith("pwd"):
+        return handle_pwd(cmd)
+    elif cmd.startswith("ls"):
+        return handle_ls(cmd)
 
 def server_run():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("127.0.0.1", 11000))
     server.listen()
 
+    setup_pyfilesystem_object()
     while True:
         print('Waiting for connections...', file=sys.stderr)
         clientsock, client_address = server.accept()  # 接続されればデータを格納
@@ -63,7 +125,7 @@ def pyfs_scratch():
         if info.is_dir:
             print("[dir]  {}".format(info.name))
         else:
-            print("[file] {}".format(info.name))
+            print("[file] {} {} MB".format(info.name, info.size / float(1024 * 1024)))
 
 if __name__ == '__main__':
     try:
